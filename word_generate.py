385b87c0b9b22d0a5c96b6548f457cfcdf165a7b
Asya Alekseeva <scarteslettt@gmail.com>
2025-12-30 21:42:46 +0300

    styles

--- word_generate.py
+++ word_generate.py
@@ -193,18 +193,16 @@ def generate_pages(data_file: str, infl_files: List[str]):
             check_page_name(data['lexeme_id'])
 
             if data["Part of Speech"].startswith("complex"):
-                complex_poses[data["lexeme_id"]] = f"This is a {data['Part of Speech']} consisting of the words: "
                 complws = ["main_word", "2nd_word", "3rd_word"]
                 complexx = []
                 for compl in complws:
                     if data[compl]:
                         if data[f'{compl}_id']:
-                            idd = data[f'{compl}_id']
-                            complexx.append('<a href="{{ base_url }}' + f'{idd}">{data[compl]}</a>')
+                            complexx.append({"word": data[compl], "word_id": data[f'{compl}_id']})
                         else:
-                            complexx.append(data[compl])
-                complexx = ", ".join(complexx)
-                complex_poses[data["lexeme_id"]] += complexx
+                            complexx.append({"word": data[compl]})
+                    complex_poses[data["lexeme_id"]] = complexx
+
 
             out_file = out_dir.joinpath(
                 f"{data['lexeme_id'].replace(', ', '-')}.html"
